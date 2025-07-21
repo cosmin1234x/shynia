80ee37a579a3d41bb7d62a3f65466c21a9331bb2
Cosmin Blidaru <112815292+cosmin1234x@users.noreply.github.com>
2025-07-21 10:22:16 +0100

    Update firebase.js

--- shyniagame/firebase.js
+++ shyniagame/firebase.js
@@ -2,20 +2,22 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.0/firebase-app.js";
 import { getDatabase, ref, set, get, onValue, update } from "https://www.gstatic.com/firebasejs/10.3.0/firebase-database.js";
 
+// firebase.js
+
 const firebaseConfig = {
-  apiKey: "AIzaSyB04bqo0BsEJaV5SymGFY64_YXT2dBd5yw",
-  authDomain: "number-guessing-game-c18bc.firebaseapp.com",
-  databaseURL: "https://number-guessing-game-c18bc-default-rtdb.firebaseio.com",
-  projectId: "number-guessing-game-c18bc",
-  storageBucket: "number-guessing-game-c18bc.firebasestorage.app",
-  messagingSenderId: "416588493658",
-  appId: "1:416588493658:web:a95fdac2e6f75d06adb963",
-  measurementId: "G-53HNXR22X2"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  databaseURL: "https://YOUR_PROJECT_ID.firebaseio.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
 };
 
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
 
-const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
 
 window.db = db;
 window.ref = ref;
